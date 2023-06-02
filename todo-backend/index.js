5b8fc3c7bc89b2c09b1eebc24b7483b653bec120
kimddddh98 <amawang11@gmail.com>
2023-06-02 16:20:47 +0900

    react 빌드파일 연결시도

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -2,22 +2,40 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors');
-app.set('port', process.env.PORT || 3000);
-app.use(cors()).use(express.static(path.join(__dirname, '../public'))).use(express.json()).use(express.urlencoded({extended:false}));
+app.set('port', process.env.PORT || 3030);
+app.use(cors()).use(express.static(path.join(__dirname, '../vite-react/dist'))).use(express.json()).use(express.urlencoded({extended:false}));
 
 let node = 'first'
-app.get('/', (req, res) => {
-  res.json({asd:'index'})
-});
+
 app.post('/test',(req,res)=>{
   node = req.body.data
   res.json({data:node})
 })
 
-app.get('/test', (req, res) => {
-  res.json({data:node})
-});
+const days = [
+  {
+    id:1,
+    day:1
+  }
+]
 
+
+
+
+app.get('/days', (req, res) => {
+  res.json(days)
+});
+app.get('/days/:id', (req, res) => {
+  const params = req.params.id
+  const data = days.filter(day=>day.id === params)
+  res.json(data)
+});
+// app.get('/test', (req, res) => {
+//   res.json({data:node})
+// });
+app.get('*', (req, res) => {
+  sendFile(path.join(__dirname, '../vite-react/dist/index.html'))  
+});
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중')
 })
