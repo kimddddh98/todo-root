e923d333754fcc37ef81f65b89f88d1adbcdfdc7
kimddddh98 <amawang11@gmail.com>
2023-06-04 12:01:53 +0900

    mongoose
    mongoose 연결 / db에 저장해보기/ 데이터 불러오고 일치하면 프론트단에 전달

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -2,25 +2,95 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors');
+const {User} = require('./models/User')
+
+const mongoose = require('mongoose')
+
+const uri = "mongodb+srv://amawang11:hZ3tHtcdsA4SEKNV@kimmongo.gq1uoem.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri,{})
+.then(()=>console.log('연결완료'))
+.catch(err=>console.log(err))
+
 app.set('port', process.env.PORT || 3030);
-app.use(cors()).use(express.static(path.join(__dirname, '../vite-react/dist'))).use(express.json()).use(express.urlencoded({extended:false}));
+app.use(cors()).use(express.static(path.join(__dirname, '../public'))).use(express.json()).use(express.urlencoded({extended:true}));
+
+
 
 let node = 'first'
 
-app.post('/test',(req,res)=>{
-  node = req.body.data
-  res.json({data:node})
+app.post('/test', async(req,res)=>{
+  const user =  new User(req.body)
+  try{
+    await user.save()
+    console.log('저장 후')
+    
+    res.status(200).json({
+      success:true
+    })
+  }catch(err){
+    return res.json({success:false,err})
+  }
+
 })
 
+app.post('/login', async(req,res)=>{
+  try{
+    const findUser =  await User.findOne({email:req.body.email})
+    if(findUser===null){
+      res.json({
+        loginSuccess:false,
+        message:'매칭안됨'
+      })
+    }
+    findUser.comparePassword(req.body.password,(err,isMatch)=>{
+      if(!isMatch) 
+        findUser.generateToken((err,user)=>{
+
+        })
+      
+      
+      return res.json({
+        loginSuccess:false,
+        message:'비밀번호가 틀렸습니다.'
+      }) 
+      res.json({
+        loginSuccess:true,
+      })
+
+    })
+
+   
+  }catch(err){
+    res.json({err})
+
+  }
+
+  // User.findOne({email:req.body.email},(err,user)=>{
+  //   if(!user){
+  //     return res.json({
+  //       loginSuccess:false,
+  //       message:'매칭안됨'
+  //     })
+  //   }
+  //   return res.json({
+  //     loginSuccess:true,
+  //   })
+  // }) 
+
+})
+
+
+
+app.get('/test', (req, res) => {
+  res.json({data:'testGet'})
+});
 const days = [
   {
     id:1,
     day:1
   }
 ]
-
-
-
 
 app.get('/days', (req, res) => {
   res.json(days)
@@ -30,11 +100,9 @@ app.get('/days/:id', (req, res) => {
   const data = days.filter(day=>day.id === params)
   res.json(data)
 });
-// app.get('/test', (req, res) => {
-//   res.json({data:node})
-// });
-app.get('*', (req, res) => {
-  sendFile(path.join(__dirname, '../vite-react/dist/index.html'))  
+
+app.get('/', (req, res) => {
+  sendFile(path.join(__dirname, '../public/index.html'))  
 });
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중')
