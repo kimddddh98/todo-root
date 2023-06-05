922f0babdf7c28731d0d51e1ce2df06e4da96740
kimddddh98 <amawang11@gmail.com>
2023-06-05 11:23:16 +0900

    빌드 수정

--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -6,7 +6,7 @@ import {resolve} from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../todo-backend/dist'
+    outDir: '../todo-backend/public'
   },
   resolve:{
     alias:[
